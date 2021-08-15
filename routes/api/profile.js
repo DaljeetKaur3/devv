af201d84b59fb375c530a1bae9cb639406b4b4a6
Daljeet kaur <daljeet1801237@gmail.com>
2021-08-15 07:27:55 +0530

    first commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const request = require('request');
+const axios = require('axios');
 const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -141,7 +142,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
   try {
-    //@todo - remove users posts
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
 
     //Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
@@ -288,49 +290,25 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   }
 });
 
-// @route   GET api/profile/github/:username
-// @desc    Get user repos from Github
-// @access  Public
-router.get('/github/:username', (req, res) => {
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', async (req, res) => {
   try {
-    const options = {
-      uri: 'https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubSecret")}',
-      method: 'GET',
-      headers: { 'user-agent': 'node.js' },
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`,
     };
 
-    request(options, (error, response, body) => {
-      if (error) console.error(error);
-
-      if (response.statusCode !== 200) {
-        return res.status(404).json({ msg: 'No Github Profile found' });
-      }
-
-      res.json(JSON.parse(body));
-    });
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    return res.status(404).json({ msg: 'No Github profile found' });
   }
 });
-// router.get('/github/:username', (req, res) => {
-//   try {
-//     const uri = encodeURI(
-//       'https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc'
-//     );
-//     console.log(uri);
-//     const headers = {
-//       'user-agent': 'node.js',
-//       Authorization: 'token ${config.get("githubToken")}',
-//     };
-
-//     const gitHubResponse = axios.get(uri, { headers });
-//     console.log(gitHubResponse);
-//     return res.json(gitHubResponse.data);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
 
 module.exports = router;
