74a89a17302760a8843ec8e210c10dc6e0c5d5c8
DaljeetKaur <87694300+DaljeetKaur3@users.noreply.github.com>
2021-08-16 07:08:22 +0530

    Update Post.js

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -11,7 +11,7 @@ import CommentItem from '../post/CommentItem';
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
-  }, [getPost]);
+  }, [getPost, match.params.id]);
 
   return loading || post === null ? (
     <Spinner />
@@ -23,9 +23,9 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
       <div className='comments'>
-        {post.comments.map((comment) => {
+        {post.comments.map((comment) => (
           <CommentItem key={comment._id} comment={comment} postId={post._id} />;
-        })}
+        ))}
       </div>
     </Fragment>
   );
